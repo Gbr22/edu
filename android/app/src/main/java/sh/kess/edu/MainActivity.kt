b04d11940f6a027a5b4cf244d980a555571f72bf
kess <git@kess.sh>
2024-01-13 17:12:42 +0100

    change package name

--- android/app/src/main/java/sh/kess/edu/MainActivity.kt
+++ android/app/src/main/java/sh/kess/edu/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.edu
+package sh.kess.edu
 
 import com.facebook.react.ReactActivity
 import com.facebook.react.ReactActivityDelegate
--- android/app/src/main/java/sh/kess/edu/MainApplication.kt
+++ android/app/src/main/java/sh/kess/edu/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.edu
+package sh.kess.edu
 
 import android.app.Application
 import com.facebook.react.PackageList
